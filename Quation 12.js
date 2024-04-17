fe1a5af9a22ae3fd02968087ba6e644405739b62
Parth bhatiya <137189036+Parth-bhatiya@users.noreply.github.com>
2024-04-17 19:20:35 +0530

    Add files via upload

---
+++ Quation 12.js
@@ -0,0 +1,13 @@
+let one = 5
+let two = 6
+let three = 19
+let four = 50
+let five = 6
+let six = 10
+let seven = 26
+let sum
+
+
+sum = one + two + three + four + five + six + seven
+
+console.log(sum);
