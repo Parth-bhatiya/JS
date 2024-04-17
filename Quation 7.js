40093587fd83104af0415f82d4d8e9dbe2c1bf22
Parth bhatiya <137189036+Parth-bhatiya@users.noreply.github.com>
2024-04-17 18:55:27 +0530

    Add files via upload

---
+++ Quation 7.js
@@ -0,0 +1,15 @@
+let a = 5
+let b = 9
+let c = 7
+let d = 4
+let e = 2
+let f = 10
+let g = 20
+let total1
+let total2
+
+total1 = a + b + c
+total2 = d + e + f + g
+
+
+console.log(total1 * total2);
