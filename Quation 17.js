01be31e4bd41f17fde4fd49c9944b000536a7667
Parth bhatiya <137189036+Parth-bhatiya@users.noreply.github.com>
2024-04-19 10:20:16 +0530

    Add files via upload

---
+++ Quation 17.js
@@ -0,0 +1,13 @@
+let x = 100
+let y = 100
+
+
+if (x > y) {
+    console.log( x,"is the largest");
+}
+else if (x == y){
+    console.log("both are equal");
+}
+else {
+    console.log(y , "is the largest");
+}
