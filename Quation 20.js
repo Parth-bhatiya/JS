223749503707c3d550946d10c2541d82ace66c1d
Parth bhatiya <137189036+Parth-bhatiya@users.noreply.github.com>
2024-04-19 15:43:56 +0530

    Add files via upload

---
+++ Quation 20.js
@@ -0,0 +1,23 @@
+let x = 4
+let y = 16
+
+if(x>y){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+
+if(x<y){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+
+if(x==y){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
