913fbb94c8f374310f3d4162fbd1216d8493ce22
Parth bhatiya <137189036+Parth-bhatiya@users.noreply.github.com>
2024-04-19 10:10:44 +0530

    Add files via upload

---
+++ Quation 16.js
@@ -0,0 +1,22 @@
+let x = 90
+let y = 12
+let z = 10
+
+
+
+if (x > y)
+{
+  if(x > z){
+    console.log(x);
+  }
+  else{
+    console.log(z);
+  }
+}
+else if(y > z)
+{
+    console.log(y);
+}
+else{
+    console.log(z);
+}
