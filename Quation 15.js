5bdec289031ebfa46743ff3e8a02a85a6def1788
Parth bhatiya <137189036+Parth-bhatiya@users.noreply.github.com>
2024-04-19 09:49:12 +0530

    Add files via upload

---
+++ Quation 15.js
@@ -0,0 +1,15 @@
+
+let n = 33
+
+if (n < 13) {
+    console.log("1 kms");
+}
+else if ((n == 13) && (n < 18)) {
+    console.log("5kms");
+}
+else if ((n >= 18) && (n <= 30)) {
+    console.log("10kms");
+}
+else {
+    console.log("invalide");
+}
