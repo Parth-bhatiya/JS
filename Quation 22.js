a28d288ac5f4a0d67ebe2054e41bd9ba8700e52a
Parth bhatiya <137189036+Parth-bhatiya@users.noreply.github.com>
2024-04-19 15:50:54 +0530

    Add files via upload

---
+++ Quation 22.js
@@ -0,0 +1,12 @@
+var Australia = 46
+var England = 46
+
+if(Australia > England){
+    console.log("Australia");
+}
+else if(Australia < England){
+    console.log("England");
+}
+else{
+    console.log("tie");
+}
